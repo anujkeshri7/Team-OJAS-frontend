46c143517b78124f60524d45c88ea58fb8870356
Anuj_keshri <anujxyt@gmail.com>
2026-02-12 22:31:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log("Anuj keshri")
+
+  return (
+    <>
+    <h1>Anuj</h1>
+    </>
+  )
+}
+
+export default App
