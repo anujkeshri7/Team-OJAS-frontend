af6c197c1dbaa0e0d20f7b5b02fafc67fd81834e
Anuj_keshri <anujxyt@gmail.com>
2026-03-08 16:28:09 +0530

    resolving error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
